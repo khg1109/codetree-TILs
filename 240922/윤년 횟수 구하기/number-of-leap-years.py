c89ac19a1500e0ffe61642475c92eb6b3f392c08
khg1109 <161204128+khg1109@users.noreply.github.com>
2024-09-22 21:03:10 +0900

    Title: 윤년 횟수 구하기, Time: 159ms, Memory: 25MB, Status: Passed - CodeTree

---
+++ 240922/윤년 횟수 구하기/number-of-leap-years.py
@@ -0,0 +1,15 @@
+n = int(input())
+
+cnt = 0
+
+for i in range(1, n+1):
+
+
+    if i % 4 == 0:
+        if i % 100 == 0:
+            if i % 400 == 0:
+                cnt += 1
+        else:
+            cnt += 1
+
+print(cnt)
