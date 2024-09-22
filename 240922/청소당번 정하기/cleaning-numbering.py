9585e404d063909c72aa1cf065da5e4b63b80d98
khg1109 <161204128+khg1109@users.noreply.github.com>
2024-09-22 20:53:47 +0900

    Title: 청소당번 정하기, Time: 152ms, Memory: 24MB, Status: Passed - CodeTree

--- 240922/청소당번 정하기/cleaning-numbering.py
+++ 240922/청소당번 정하기/cleaning-numbering.py
@@ -8,18 +8,15 @@ n = int(input())
 
 
 
-for i in range(n):
+for i in range(1,n+1):
 
-    if i == 0:
-        continue
+
     if i % 12 == 0:
         bathroom += 1
     elif i % 3 == 0:
         corridor += 1
     elif i % 2 == 0:
         classroom += 1
-
-
 
 
 print("%d %d %d" % (classroom, corridor, bathroom))
