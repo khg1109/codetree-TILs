20d5b93841685487217afa36c03a97aa8dc0b850
khg1109 <161204128+khg1109@users.noreply.github.com>
2024-09-22 17:58:47 +0900

    Title: a/b 출력, Time: 121ms, Memory: 24MB, Status: Passed - CodeTree

--- 240922/a/b 출력/a-divide-b.py
+++ 240922/a/b 출력/a-divide-b.py
@@ -3,8 +3,11 @@ n = list(map(int, input().split()))
 
 
 print("%d." % (n[0] // n[1]), end = "")
-n[0] *= 10
+
+n[0] %= n[1]
+
 for i in range(20):
+
+    n[0] *= 10
     print(n[0] // n[1] , end ="")
     n[0] %= n[1]
-    n[0] *= 10
