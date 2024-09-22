66d52ac00294377026312e0623699626bc6010c6
khg1109 <161204128+khg1109@users.noreply.github.com>
2024-09-22 18:15:42 +0900

    Title: 숫자의 증감, Time: 216ms, Memory: 25MB, Status: Passed - CodeTree

--- 240922/숫자의 증감/increasing-and-decreasing-numbers.py
+++ 240922/숫자의 증감/increasing-and-decreasing-numbers.py
@@ -5,5 +5,5 @@ if n[0] == 'A':
     for i in range(1,int(n[1])+1):
         print(i, end = " ")
 else:
-    for i in range(int(n[1], 0, -1)):
+    for i in range(int(n[1]), 0, -1):
         print(i, end = " ")
