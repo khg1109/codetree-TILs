4812f367a25a393c15fd9b48ad25e1dd880a1622
khg1109 <161204128+khg1109@users.noreply.github.com>
2024-09-22 18:12:21 +0900

    Title: 숫자의 증감, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240922/숫자의 증감/increasing-and-decreasing-numbers.py
@@ -0,0 +1,12 @@
+n = input().split()
+
+
+if n[0] == 'A':
+    for i in range(1,int(n[1])+1):
+        print(i, end = " ")
+
+
+
+if n[0] == 'D':
+    for i in range(int(n[1], 0, -1)):
+        print(i, end = " ")
