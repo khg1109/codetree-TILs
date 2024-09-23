665f82db52927d14693ba85f40ec8c3085cd47ce
khg1109 <161204128+khg1109@users.noreply.github.com>
2024-09-23 23:33:33 +0900

    Title: ab 사이에 있는 c, Time: 119ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240923/ab 사이에 있는 c/c-between-a-and-b.py
@@ -0,0 +1,14 @@
+satisfied = False
+
+n = list(map(int, input().split()))
+
+for i in range(n[0], n[1]+1):
+    
+    if i % n[2] == 0:
+        satisfied = True
+    
+
+if satisfied:
+    print("YES")
+else:
+    print("NO")
