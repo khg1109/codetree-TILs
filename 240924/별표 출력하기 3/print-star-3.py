7bef8e985a76a39f9b5a4f5ce8f79434c2eda86a
khg1109 <161204128+khg1109@users.noreply.github.com>
2024-09-24 10:37:42 +0900

    Title: 별표 출력하기 3, Time: 117ms, Memory: 24MB, Status: Passed - CodeTree

--- 240924/별표 출력하기 3/print-star-3.py
+++ 240924/별표 출력하기 3/print-star-3.py
@@ -5,7 +5,7 @@ for i in range(n):
     for j in range(i):
         print(" ",  end = " ")
 
-    for j in range((n+(n-1))-(i*2)):
+    for j in range(((n*2)-1)-(i*2)):
         print("*", end = " ")
 
     print()
