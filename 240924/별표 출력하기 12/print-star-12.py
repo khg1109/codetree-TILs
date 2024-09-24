9bf4c92ea4fe63ceb889a9c53beec6741c45f759
khg1109 <161204128+khg1109@users.noreply.github.com>
2024-09-24 14:44:12 +0900

    Title: 별표 출력하기 12, Time: 145ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240924/별표 출력하기 12/print-star-12.py
@@ -0,0 +1,12 @@
+n = int(input())
+
+
+for i in range(n):
+    for j in range(n):
+
+        if (i == 0) or ((j % 2 == 1) and (i <= j)):
+            print("*", end = " ")
+        else:
+            print(" ", end = " ")
+
+    print()
