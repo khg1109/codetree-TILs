d16b2a7726d178cb989efe239d191c7bf5358706
khg1109 <161204128+khg1109@users.noreply.github.com>
2024-09-25 16:28:27 +0900

    Title: 정상적인 구구단, Time: 209ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240925/정상적인 구구단/normal-multiple.py
@@ -0,0 +1,14 @@
+n = int(input())
+
+
+for i in range(1,n+1):
+
+    for j in range(1,n+1):
+
+        if j == n:
+            print("%d * %d = %d" % (i,j,i*j), end = " ")
+
+        else:
+            print("%d * %d = %d," % (i,j,i*j), end = " ")
+
+    print()
