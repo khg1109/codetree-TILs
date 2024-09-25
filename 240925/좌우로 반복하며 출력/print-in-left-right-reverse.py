b1819801858005f22a900b4b52f6ad45140ad5c8
khg1109 <161204128+khg1109@users.noreply.github.com>
2024-09-25 12:53:39 +0900

    Title: 좌우로 반복하며 출력, Time: 120ms, Memory: 24MB, Status: Passed - CodeTree

--- 240925/좌우로 반복하며 출력/print-in-left-right-reverse.py
+++ 240925/좌우로 반복하며 출력/print-in-left-right-reverse.py
@@ -3,9 +3,9 @@ n = int(input())
 for i in range(n):
     
     if i % 2 ==0:
-        for j in range(1,n+1):
-            print(j, end = "")
+        for j in range(n):
+            print(j+1, end = "")
     else:
-        for j in range(n,0,-1):
-            print(j, end = "")
+        for j in range(n):
+            print((n)-j, end = "")
     print()
