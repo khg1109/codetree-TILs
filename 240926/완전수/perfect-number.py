cd97373753bda93b5738059786053679c64f56a2
khg1109 <161204128+khg1109@users.noreply.github.com>
2024-09-26 22:24:06 +0900

    Title: 완전수, Time: 106ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240926/완전수/perfect-number.py
@@ -0,0 +1,18 @@
+n = list(map(int, input().split()))
+
+
+sum = 0
+cnt = 0
+for i in range(n[0]+1,n[1]+1):
+    for j in range(1, i-1):
+        
+        if i % j == 0:
+            sum += j
+
+    
+    if sum == i:
+        cnt += 1
+        
+    sum = 0
+
+print(cnt)
