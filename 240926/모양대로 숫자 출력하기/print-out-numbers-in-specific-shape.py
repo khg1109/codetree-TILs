31e03249e54e509865ef46223b2f7e45eb19efa3
khg1109 <161204128+khg1109@users.noreply.github.com>
2024-09-26 13:55:05 +0900

    Title: 모양대로 숫자 출력하기, Time: 151ms, Memory: 24MB, Status: Passed - CodeTree

--- 240926/모양대로 숫자 출력하기/print-out-numbers-in-specific-shape.py
+++ 240926/모양대로 숫자 출력하기/print-out-numbers-in-specific-shape.py
@@ -1,10 +1,10 @@
 n = int(input())
 
 
-for i in range(n):
+for i in range(n,0,-1):
 
-    for j in range(i):
-        print(" ", end = " ")
     for j in range(n-i):
-        print(n-j, end = " ")
+        print(" ", end = " ")
+    for j in range(i):
+        print(i-j, end = " ")
     print()
