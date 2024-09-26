aa8a2d6228c1f6bd4ec41565cc2317bbd80134ac
khg1109 <161204128+khg1109@users.noreply.github.com>
2024-09-26 23:09:32 +0900

    Title: 소수 구하기, Time: 112ms, Memory: 26MB, Status: Passed - CodeTree

---
+++ 240926/소수 구하기/get-prime.py
@@ -0,0 +1,19 @@
+n = int(input())
+
+distinction = True
+
+for i in range(1, n+1):
+
+    if i == 1:
+        continue
+
+    for j in range(1, i):
+        if j == 1 :
+            continue
+        elif i % j == 0:
+            distinction = False
+
+    if distinction:
+        print(i, end = " ")
+    
+    distinction = True
