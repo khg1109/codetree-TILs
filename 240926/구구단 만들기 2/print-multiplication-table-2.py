05fc5ecb8429af991d23fefb5ea0095f136aa3c0
khg1109 <161204128+khg1109@users.noreply.github.com>
2024-09-26 13:23:09 +0900

    Title: 구구단 만들기 2, Time: 112ms, Memory: 24MB, Status: Passed - CodeTree

--- 240926/구구단 만들기 2/print-multiplication-table-2.py
+++ 240926/구구단 만들기 2/print-multiplication-table-2.py
@@ -4,7 +4,10 @@ n = list(map(int, input().split()))
 
 for i in range(2, 10, 2):
 
-    for j in range(n[1], n[0]-1):
-        
-        print("%d * %d = %d / " % (j, i, j*i))
+    for j in range(n[1], n[0]-1,-1):
+
+        if j == n[0]:
+            print("%d * %d = %d " % (j, i, j*i), end = "")
+        else:    
+            print("%d * %d = %d / " % (j, i, j*i), end = "")
     print()
