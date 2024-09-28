5121850bb563ec0e0d7b72c90cc9aa762139aedb
khg1109 <161204128+khg1109@users.noreply.github.com>
2024-09-28 23:15:49 +0900

    Title: 1-9 개수 세기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240928/1-9 개수 세기/count-one-to-nine.c
+++ 240928/1-9 개수 세기/count-one-to-nine.c
@@ -2,24 +2,27 @@
 
 int main() {
 
-    int n;
+	int n;
+
     scanf("%d", &n);
 
-	int arr[n];
-	int cntArr[10] = {0,};
+    int arr[n];
+    int cntArr[10] = {0,};
 
-	for (int i = 0; i < n; i++) {
-		scanf("%d", &arr[i]);
-	}
+    for(int i = 0; i < n; i++){
+        scanf("%d", &arr[i]);
+    }
+
+    for(int i = 0; i < n; i++){
+        cntArr[arr[i]]++;
+    }
+
+    for(int i = 1; i < 10; i++){
+        printf("%d\n", cntArr[i]);
+
+    }
 
 
-	for (int i = 0; i < n; i++) {
-		cntArr[arr[i]]++;
-	}
-
-	for (int i = 1; i < 10; i++) {
-		printf("%d\n", cntArr[i]);
-	}
 
 	return 0;
 
