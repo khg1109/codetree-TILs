5a9926a35c7c130e2ed642761c11fc5f4264bdbc
khg1109 <161204128+khg1109@users.noreply.github.com>
2024-09-29 17:00:39 +0900

    Title: 나눗셈의 나머지, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240929/나눗셈의 나머지/remainder-of-division.c
+++ 240929/나눗셈의 나머지/remainder-of-division.c
@@ -1,31 +1,26 @@
 #include <stdio.h>
 
 int main() {
+    int a, b;
 
-    int a;
-    int b;
-
+   
     scanf("%d %d", &a, &b);
 
-    int cnt = 0;
-    int cntArr[100] = {0};
+    int cntArr[100] = {0}; 
+    int sumOfSquares = 0;
 
-    while(a > 1){
-        cnt++;
-        a /= b;
-        cntArr[a % b]++;
-        
+
+    while (a > 1) {
+         
+        cntArr[a%b]++; 
+        a /= b; 
     }
-    int sum = 0;
-    int sum2 = 0;
-
-    for(int i = 0; i < cnt; i++){
-      sum += cntArr[i] * cntArr[i];
-
+   
+    for (int i = 0; i < b; i++) {
+        sumOfSquares += cntArr[i] * cntArr[i];
     }
 
-    printf("%d", sum);
+    printf("%d\n", sumOfSquares);
 
-    // 여기에 코드를 작성해주세요.
     return 0;
 }
