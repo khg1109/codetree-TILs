a06f471a8fda984f399519492ef5a73d92b92da1
khg1109 <161204128+khg1109@users.noreply.github.com>
2024-09-29 19:02:47 +0900

    Title: 특정 위치의 문자, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240929/특정 위치의 문자/char-in-specific-location.c
+++ 240929/특정 위치의 문자/char-in-specific-location.c
@@ -1,19 +1,25 @@
 #include <stdio.h>
-
+#include <stdbool.h>
 int main() {
-    char arr[6] = {'l', 'E', 'B', 'R', 'O', 'S'};
-    char ch;
+  char arr[6] = {'L', 'E', 'B', 'R', 'O', 'S'};
+  char ch;
     
   
-    scanf(" %c", &ch); 
- 
+  scanf(" %c", &ch); 
+  bool inspection = true; 
 
-    for (int i = 0; i < 6; i++) {
-      if (ch == arr[i]) { 
-        printf("%d\n", i); 
-        break; 
-      }
+
+  for (int i = 0; i < 6; i++) {
+    if(arr[i] == ch){
+      printf("%d", i);
+      inspection = false;
     }
+  }
+
+  if(inspection){
+    printf("None");
+
+  }
 
     return 0;
 }
