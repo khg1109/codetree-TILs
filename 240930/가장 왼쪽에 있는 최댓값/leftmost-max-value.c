35967771a7c3f98970ff77bcd260da7aba568b85
khg1109 <161204128+khg1109@users.noreply.github.com>
2024-09-30 16:30:33 +0900

    Title: 가장 왼쪽에 있는 최댓값, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240930/가장 왼쪽에 있는 최댓값/leftmost-max-value.c
+++ 240930/가장 왼쪽에 있는 최댓값/leftmost-max-value.c
@@ -6,22 +6,21 @@ int main() {
 
     int n;
     scanf("%d", &n);
-    int arr[100];
+    int arr[1000];
     int maxDex = 0;
 
     for(int i = 0; i < n; i++){
         scanf("%d", &arr[i]);
     }
-    bool inspection = true;
 
-    int max = arr[0];
-    int a = 0;
+
+    int prev_max_idx = n;
 
     while(1){
         
         int max_idx = 0;
 
-        for(int i = 1; i < n; i++){
+        for(int i = 1; i < prev_max_idx; i++){
             if(arr[i] > arr[max_idx]){
                 max_idx = i;
             }
@@ -33,7 +32,7 @@ int main() {
             break;
         }
 
-        n = max_idx;
+        prev_max_idx = max_idx;
 
         
     }
