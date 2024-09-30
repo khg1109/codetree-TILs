f468787ea5d3a5864df124f97a9e0562b061527a
khg1109 <161204128+khg1109@users.noreply.github.com>
2024-09-30 15:51:40 +0900

    Title: 가장 왼쪽에 있는 최댓값, Time: 15ms, Memory: 0MB, Status: Runtime Error - CodeTree

---
+++ 240930/가장 왼쪽에 있는 최댓값/leftmost-max-value.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int main() {
+
+    int n;
+    scanf("%d", &n);
+    int arr[100];
+    int maxDex = 0;
+
+
+    for(int i = 0; i < n; i++){
+        scanf("%d", &arr[i]);
+    }
+
+    bool inspection = true;
+
+    int max = arr[0];
+    while(inspection){
+        inspection = false;
+        for(int i = 0; i < n; i++){
+            if(max < arr[i]){
+                max = arr[i];
+                maxDex = i;
+                inspection = true;
+            }
+            //printf("인덱스: %d %d %d\n", i,max,maxDex);
+        }
+        max = arr[0];
+        n = maxDex;
+        printf("%d ", maxDex+1);
+        maxDex = 0;
+        
+
+    }
+    
+
+    return 0;
+}
