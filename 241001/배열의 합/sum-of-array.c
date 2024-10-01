22119b37c544cf4b980e284a2157106796815c55
khg1109 <161204128+khg1109@users.noreply.github.com>
2024-10-01 12:50:51 +0900

    Title: 배열의 합, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241001/배열의 합/sum-of-array.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main() {
+
+
+    int arr[4][4];
+
+    for(int i = 0; i < 4; i++){
+        for(int j = 0; j < 4; j++){
+            scanf("%d", &arr[i][j]);
+        }
+    }
+
+    int sum = 0;
+
+    for(int i = 0; i < 4; i++){
+        sum = 0;
+        for(int j = 0; j < 4; j++){
+            sum += arr[i][j];
+        }
+
+        printf("%d\n", sum);
+    }
+
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
