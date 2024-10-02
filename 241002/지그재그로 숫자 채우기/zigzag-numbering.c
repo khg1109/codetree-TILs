0f52dd02018768076773eab86b71893a093c236b
khg1109 <161204128+khg1109@users.noreply.github.com>
2024-10-02 17:35:14 +0900

    Title: 지그재그로 숫자 채우기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241002/지그재그로 숫자 채우기/zigzag-numbering.c
+++ 241002/지그재그로 숫자 채우기/zigzag-numbering.c
@@ -10,16 +10,29 @@ int main() {
     int cnt = 0;
 
 
+    
+    for (int i = 0; i < m; i++){
 
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < m; j++) {
-           arr[i][j] = cnt;
-           cnt++;
-           printf("%d", arr[i][j]);
+        if(i % 2 == 0){
+            for(int j = 0; j < n; j++){
+                arr[j][i] = cnt;
+                cnt++;
+            }
         }
-        printf("\n");
-
+        else{
+            for(int j = 0; j < n; j++){
+                arr[n-1-j][i] = cnt;
+                cnt++;
+            }
+        }
     }
 
+
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < m; j++){
+            printf("%d ", arr[i][j]);
+        }
+        printf("\n");
+    }
     return 0;
 }
