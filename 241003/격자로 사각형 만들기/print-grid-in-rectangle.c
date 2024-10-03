6ffb5b3a9a16ee8226ee9d0e0268e3b878859bf9
khg1109 <161204128+khg1109@users.noreply.github.com>
2024-10-03 19:35:46 +0900

    Title: 격자로 사각형 만들기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241003/격자로 사각형 만들기/print-grid-in-rectangle.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+int main() {
+
+    int n;
+    scanf("%d", &n);
+
+    int arr[100][100] = {0};
+
+
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < n; j++){
+            if((i == 0) || (j == 0)){
+                arr[i][j] = 1;
+
+            }
+        }
+    }
+
+    for(int i = 1; i < n; i++){
+        for(int j = 1; j < n; j++){
+            arr[i][j] = arr[i-1][j-1]+arr[i-1][j]+arr[i][j-1];
+
+        }
+    
+    }
+
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < n; j++){
+            printf("%d ", arr[i][j]);
+        }
+        printf("\n");
+
+    
+    }
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
