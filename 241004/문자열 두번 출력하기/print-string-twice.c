74cf8797d3d7e79ebf67396205f71097317d2a13
khg1109 <161204128+khg1109@users.noreply.github.com>
2024-10-04 10:15:35 +0900

    Title: 문자열 두번 출력하기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241004/문자열 두번 출력하기/print-string-twice.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+
+    char str[100];
+
+    scanf("%s", &str);
+
+    printf("%d\n", str);
+    printf("%d\n", str);
+
+
+
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
