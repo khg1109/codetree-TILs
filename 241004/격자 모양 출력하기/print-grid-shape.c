fa9e190fc60f6f991f267f658b4a03f9b185808b
khg1109 <161204128+khg1109@users.noreply.github.com>
2024-10-04 10:07:32 +0900

    Title: 격자 모양 출력하기, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 241004/격자 모양 출력하기/print-grid-shape.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main() {
+
+    int n, m;
+
+    scanf("%d %d", &n, &m);
+
+    arr[10][10];
+
+    for(int i = 0; i < m; i++){
+        int c,r;
+
+        scanf("%d %d", &c, &r);
+
+        arr[c-1][r-1] = c*r;
+    }
+
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < n: j++){
+            printf("%d", arr[i][j]);
+        }
+        printf("\n");
+        
+    }
+
+
+
+
+
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
