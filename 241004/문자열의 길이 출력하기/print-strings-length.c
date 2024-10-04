6becccd6dcacd3d31d1db54012017335ea715b01
khg1109 <161204128+khg1109@users.noreply.github.com>
2024-10-04 10:21:40 +0900

    Title: 문자열의 길이 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241004/문자열의 길이 출력하기/print-strings-length.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+
+    char str1[100];
+    char str2[100];
+
+    scanf("%s %s", str1, str2);
+
+    printf("%d", strlen(str1) + strlen(str2));
+
+
+
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
