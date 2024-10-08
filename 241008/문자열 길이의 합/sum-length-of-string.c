529a50c878899d2208106e42439448be788623b5
khg1109 <161204128+khg1109@users.noreply.github.com>
2024-10-08 14:39:54 +0900

    Title: 문자열 길이의 합, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241008/문자열 길이의 합/sum-length-of-string.c
+++ 241008/문자열 길이의 합/sum-length-of-string.c
@@ -18,7 +18,7 @@ int main() {
 
     for(int i = 0; i < n; i++){
         for(int j = 0; j < strlen(arr[i]); j++){
-            if((int)arr[i][0] == 97){
+            if((j == 0) && (arr[i][j] == 'a')){
                 cnta++;
             }
             cnt++;
