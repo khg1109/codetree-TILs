7a8667d41cc65ca1d36e5c86dfb84ac33158f322
khg1109 <161204128+khg1109@users.noreply.github.com>
2024-10-08 14:44:39 +0900

    Title: 단어로 구분하기 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241008/단어로 구분하기 2/separate-words-with-words-2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+
+
+    char arr[10][100];
+    for(int i = 0; i < 10; i++){
+        scanf("%s", &arr[i]);
+
+        if(i % 2 == 1){
+            printf("%s\n", arr[i]);
+        }
+    }
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
