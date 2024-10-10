409818eb134dc3c674268104112858725f7bc76c
khg1109 <161204128+khg1109@users.noreply.github.com>
2024-10-10 14:27:05 +0900

    Title: 문자열 전부 붙이기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241010/문자열 전부 붙이기/paste-all-string.c
+++ 241010/문자열 전부 붙이기/paste-all-string.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 
 int main() {
 
@@ -9,11 +10,18 @@ int main() {
 
     for(int i = 0; i < n; i++){
         scanf("%s", &arr[i]);
+    }
 
 
-        printf("%s\n", arr[i]);
+    char str[100];
+
+    for(int i = 0; i < n; i++){
+        strcat(str, arr[i]);
 
     }
+
+    printf("%s", str);
+
     // 여기에 코드를 작성해주세요.
     return 0;
 }
