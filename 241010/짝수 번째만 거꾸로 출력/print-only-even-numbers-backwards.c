1463cd8619d3c2fe93c8baf95b494cac53b0e502
khg1109 <161204128+khg1109@users.noreply.github.com>
2024-10-10 13:55:31 +0900

    Title: 짝수 번째만 거꾸로 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241010/짝수 번째만 거꾸로 출력/print-only-even-numbers-backwards.c
+++ 241010/짝수 번째만 거꾸로 출력/print-only-even-numbers-backwards.c
@@ -4,24 +4,23 @@
 int main() {
     char str[100];
 
-    // 문자열 입력
-    scanf("%s", str);
-    
-    char evenChars[100]; // 짝수 번째 문자를 저장할 배열
-    int evenCount = 0;
 
-    // 짝수 번째 문자만 추출
-    for (int i = 0; i < strlen(str); i++) {
-        if (i % 2 == 0) {
-            evenChars[evenCount] = str[i];
-            evenCount++;
+    scanf("%s", str);
+    char str2[100]; 
+
+    int cnt = 0;
+    
+    for(int i = 0; i < strlen(str); i++){
+        if(i % 2 == 1){
+            str2[cnt++] = str[i];
         }
     }
 
-    // 짝수 번째 문자를 역순으로 출력
-    for (int i = evenCount - 1; i >= 0; i--) {
-        printf("%c", evenChars[i]);
+
+    for(int i = cnt - 1; i >= 0; i--){
+        printf("%c", str2[i]);
     }
+
 
     return 0;
 }
