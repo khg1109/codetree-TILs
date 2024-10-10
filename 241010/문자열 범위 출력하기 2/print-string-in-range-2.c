1f71e3872485dcfc65d213b803e103c16b4a30ac
khg1109 <161204128+khg1109@users.noreply.github.com>
2024-10-10 13:17:48 +0900

    Title: 문자열 범위 출력하기 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241010/문자열 범위 출력하기 2/print-string-in-range-2.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+
+
+    char str[100];
+    int n;
+
+    scanf("%s", &str);
+    scanf("%d", &n);
+
+
+    for(int i = 0; i < 11; i++){
+        printf("%c", str[n-i+2]);
+    }
+
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
