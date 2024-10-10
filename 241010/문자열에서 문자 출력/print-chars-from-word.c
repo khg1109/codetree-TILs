7fbe11ad6ab8267247eb0d14bfc9eab2f75fc8ea
khg1109 <161204128+khg1109@users.noreply.github.com>
2024-10-10 13:08:12 +0900

    Title: 문자열에서 문자 출력, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241010/문자열에서 문자 출력/print-chars-from-word.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+
+    char str[11];
+
+    scanf("%s", str);
+
+    for(int i = 0; str[i] != '\0'; i++){
+        printf("%c\n", str[i]);
+
+    }
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
