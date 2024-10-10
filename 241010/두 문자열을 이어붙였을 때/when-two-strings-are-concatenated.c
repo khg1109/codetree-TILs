55a715d46558930b252d599736b7fa46407b8bf6
khg1109 <161204128+khg1109@users.noreply.github.com>
2024-10-10 14:56:43 +0900

    Title: 두 문자열을 이어붙였을 때, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241010/두 문자열을 이어붙였을 때/when-two-strings-are-concatenated.c
+++ 241010/두 문자열을 이어붙였을 때/when-two-strings-are-concatenated.c
@@ -1,35 +1,31 @@
 #include <stdio.h>
 #include <string.h>
 
-
 int main() {
-
-    char AB[100] = "";
-    char BA[100] = "";
-
-
     char A[100];
     char B[100];
 
-    scanf("%s", &A);
-    scanf("%s", &B);
+    // 문자열 입력
+    scanf("%s", A);
+    scanf("%s", B);
 
+    // AB와 BA를 저장할 충분한 크기의 배열
+    char AB[200];
+    char BA[200];
 
-    strcat(AB, A);
-    strcat(AB, B);
+    // 문자열 연결
+    strcpy(AB, A); // A를 AB에 복사
+    strcat(AB, B); // B를 AB에 이어붙임
 
-    strcat(BA, B);
-    strcat(BA, A);
+    strcpy(BA, B); // B를 BA에 복사
+    strcat(BA, A); // A를 BA에 이어붙임
 
-
-    if(strcmp(AB, BA) == 0){
-        printf("true");
+    // 문자열 비교
+    if (strcmp(AB, BA) == 0) {
+        printf("true\n");
+    } else {
+        printf("false\n");
     }
-    else{
-        printf("false");
-    }
 
-
-    // 여기에 코드를 작성해주세요.
     return 0;
 }
