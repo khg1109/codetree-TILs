adad4c2d446e13b375e9ae4035753beac8e3144c
khg1109 <161204128+khg1109@users.noreply.github.com>
2024-10-27 17:41:36 +0900

    Title: 두 번째를 첫 번째로, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241027/두 번째를 첫 번째로/second-to-first.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+
+    char str[100];
+
+
+    scanf("%s", str);
+
+
+    char ch1 = str[0];
+    char ch2 = str[1];
+
+    for(int i = 0; i < strlen(str); i++){
+        if(ch2 == str[i]){
+            str[i] = ch1;
+
+        }
+    }
+
+
+
+    printf("%s", str);
+    
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
