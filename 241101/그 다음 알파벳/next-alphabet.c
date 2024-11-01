daf8042baa9c103df59a673a2aae9f7070ef4234
khg1109 <161204128+khg1109@users.noreply.github.com>
2024-11-01 10:39:41 +0900

    Title: 그 다음 알파벳, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241101/그 다음 알파벳/next-alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+
+
+    char c;
+    scanf("%c", &c);
+
+    int c1 = c + 1;
+
+
+
+    if(c1 == 123){
+        c1 = 97;
+    }
+    printf("%c", c1);
+
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
