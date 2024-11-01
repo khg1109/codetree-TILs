35ef7e3c4140144a4bc3e4f17b2c388602077734
khg1109 <161204128+khg1109@users.noreply.github.com>
2024-11-01 10:39:29 +0900

    Title: 아스키코드표 맞추기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241101/아스키코드표 맞추기/chart-of-ASCII.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+
+
+    int arr[5];
+
+    for(int i = 0; i < 5; i++){
+        scanf("%d", &arr[i]);
+        printf("%c ", arr[i]);
+    }
+
+
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
