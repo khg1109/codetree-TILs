545751297c611d2b973aa35170ca6cfd0486781b
khg1109 <161204128+khg1109@users.noreply.github.com>
2024-11-01 11:15:58 +0900

    Title: 정수만 추출하기, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241101/정수만 추출하기/extract-only-integers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+
+    char str1[10];
+    char str2[10];
+
+
+    scanf("%s %s", str1, str2);
+
+    printf("%s \n%s",str1, str2);
+
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
