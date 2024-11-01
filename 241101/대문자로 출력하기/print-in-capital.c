5a717edc01a450d269b3aa9bf8bad1af323b3af4
khg1109 <161204128+khg1109@users.noreply.github.com>
2024-11-01 10:06:51 +0900

    Title: 대문자로 출력하기, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241101/대문자로 출력하기/print-in-capital.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <string.h>
+
+
+int main() {
+
+    char str[101];
+
+    scanf("%s", str);
+    
+
+    for(int i = 0; i < strlen(str); i++){
+
+
+        if((str[i] >= 65 && str[i] <= 90) || (str[i] >= 97 && str[i] <= 122)){
+            if (str[i] >= 97 && str[i] <= 122) {
+                printf("%c", str[i] - 'A' + 'a'); 
+            } else {
+                printf("%c", str[i]);
+            }
+        }
+
+
+    }
+
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
