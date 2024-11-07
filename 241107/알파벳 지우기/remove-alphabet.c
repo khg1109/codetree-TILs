03b03836b5adb14136df1e414d1af74c2d0d58e9
khg1109 <161204128+khg1109@users.noreply.github.com>
2024-11-07 11:12:43 +0900

    Title: 알파벳 지우기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 241107/알파벳 지우기/remove-alphabet.c
+++ 241107/알파벳 지우기/remove-alphabet.c
@@ -1,4 +1,7 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
 
 int main() {
 
@@ -7,14 +10,42 @@ int main() {
 
 
     scanf("%s %s", str1, str2);
-    printf("%s %s", str1, str2);
 
 
     char num1[10];
     char num2[10];
 
+    int cnt1 = 0;
+    int cnt2 = 0;
 
-    for(int i = 0; i < )
-    // 여기에 코드를 작성해주세요.
+    int len1 = strlen(str1);
+    int len2 = strlen(str2);
+    
+    for(int i = 0; i < len1; i++){
+        
+        if(str1[i] >= '0' && str1[i] <= '9'){
+            num1[cnt1] = str1[i];
+            cnt1++;
+        }   
+    }
+    num1[cnt1] = '\0';
+
+
+    for(int i = 0; i < len2; i++){
+        
+        if(str2[i] >= '0' && str2[i] <= '9'){
+            num2[cnt2] = str2[i];
+            cnt2++;
+        }   
+    }
+    num2[cnt2] = '\0';
+
+    printf("%d", atoi(num1) + atoi(num2));
+    
+
+    
+
+
+
     return 0;
 }
