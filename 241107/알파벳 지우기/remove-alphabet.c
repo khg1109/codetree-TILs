d1a1c23a584f6083b20208e8cbb3ea7682aa06e0
khg1109 <161204128+khg1109@users.noreply.github.com>
2024-11-07 10:43:36 +0900

    Title: 알파벳 지우기, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 241107/알파벳 지우기/remove-alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+
+    char str1[10];
+    char str2[10];
+
+
+    scanf("%s %s", str1, str2);
+    printf("%s %s", str1, str2);
+
+
+    char num1[10];
+    char num2[10];
+
+
+    for(int i = 0; i < )
+    // 여기에 코드를 작성해주세요.
+    return 0;
+}
