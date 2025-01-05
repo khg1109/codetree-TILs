d1cfc4e0d1b45fe799c160ac8665c40668ae18a6
khg1109 <161204128+khg1109@users.noreply.github.com>
2025-01-05 20:19:33 +0900

    Title: 반복 출력하기, Time: 119ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 250105/반복 출력하기/repeated-output.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int rowNum = sc.nextInt();
+        printN(rowNum);
+
+
+        // Please write your code here.
+    }
+
+    static void printN(int n){
+
+        for(int i = 0; i < n; i++){
+            System.out.println("12345^&*()_");
+            
+        }
+    }
+}
