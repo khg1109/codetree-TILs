17edd3039a312c8164d38f1ffbf01271dca35f8d
khg1109 <161204128+khg1109@users.noreply.github.com>
2025-01-07 14:38:05 +0900

    Title: 함수를 이용한 합과 소수 판별, Time: 133ms, Memory: 10MB, Status: Passed - CodeTree

--- 250107/함수를 이용한 합과 소수 판별/use-functions-to-determine-sums-and-decimals.java
+++ 250107/함수를 이용한 합과 소수 판별/use-functions-to-determine-sums-and-decimals.java
@@ -5,45 +5,38 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int a = sc.nextInt();
-        int b = sc.nextInt();
+        // 사용자 입력
+        int start = sc.nextInt(); // 시작 범위
+        int end = sc.nextInt();   // 끝 범위
 
-        int c = 0;
+        int count = 0; // 조건을 만족하는 숫자의 개수
 
-        for(int i = a; i <= b; i++){
-            c +=isPrime(i);
+        for (int num = start; num <= end; num++) {
+            count += isSpecialPrime(num); // 조건에 따라 누적
         }
-        System.out.printf("%d", c);
-
-        // Please write your code here.
+        System.out.printf("%d", count);
     }
 
-
-    static int isPrime(int i){
-        
-        for(int j = 2; j < i; j++){
-            if(i % j == 0){
-                return 0;
+    static int isSpecialPrime(int num) {
+        // 소수 여부 판별
+        for (int divisor = 2; divisor < num; divisor++) {
+            if (num % divisor == 0) {
+                return 0; // 소수가 아님
             }
         }
-        
-        int sum = 0;
-        int a = 0;
-        while(i > 0){
-            a = i % 10;
-            sum += a;
-            i /= 10;
 
+        int digitSum = 0; // 자릿수 합
+        int current = num;
+        while (current > 0) {
+            digitSum += current % 10; // 각 자릿수 더하기
+            current /= 10;
         }
 
-        if(sum % 2 == 0){
-            return 1;
+        // 자릿수 합이 짝수인지 확인
+        if (digitSum % 2 == 0) {
+            return 1; // 조건 만족
+        } else {
+            return 0; // 조건 불만족
         }
-        else{
-            return 0;
-            
-        }
-
-     
     }
 }
