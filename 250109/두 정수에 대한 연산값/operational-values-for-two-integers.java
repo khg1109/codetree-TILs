d715c51b43c5545b867c606d6a6b05f325eb08f6
khg1109 <161204128+khg1109@users.noreply.github.com>
2025-01-09 13:19:54 +0900

    Title: 두 정수에 대한 연산값, Time: 125ms, Memory: 10MB, Status: Passed - CodeTree

--- 250109/두 정수에 대한 연산값/operational-values-for-two-integers.java
+++ 250109/두 정수에 대한 연산값/operational-values-for-two-integers.java
@@ -1,36 +1,40 @@
 import java.util.Scanner;
 
+class IntWrapper{
+    int value;
+
+    public IntWrapper(int value){
+        this.value = value;
+    }
+}
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
 
-        int a = sc.nextInt();
-        int b = sc.nextInt();
+        int n = sc.nextInt();
+        int m = sc.nextInt();
 
+        IntWrapper a = new IntWrapper(n);
+        IntWrapper b = new IntWrapper(m);
 
         function(a, b);
-        // Please write your code here.
+
+
+        System.out.printf("%d %d", a.value, b.value);
+        
     }
 
-    static void function(int a, int b){
-        int min = 0;
-        int max = 0;
+    static void function(IntWrapper a, IntWrapper b){
 
-
-        if(a < b){
-            min = a;
-            max = b;
+        if(a.value < b.value){
+            a.value *= 2;
+            b.value += 25;
         }
         else{
-            min = b;
-            max = a;
-        }
-    
-        max += 25;
-        min *= 2;
-
-        System.out.printf("%d %d", min, max);
-        
-        
+            b.value *= 2;
+            a.value += 25;
+        }    
     }
+
 }
