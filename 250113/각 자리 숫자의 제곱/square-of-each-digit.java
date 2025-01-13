e3b367dcc629ab38abe19c5d315997e1460cac4c
khg1109 <161204128+khg1109@users.noreply.github.com>
2025-01-13 13:15:51 +0900

    Title: 각 자리 숫자의 제곱, Time: 131ms, Memory: 10MB, Status: Passed - CodeTree

--- 250113/각 자리 숫자의 제곱/square-of-each-digit.java
+++ 250113/각 자리 숫자의 제곱/square-of-each-digit.java
@@ -5,18 +5,16 @@ public class Main {
         Scanner sc = new Scanner(System.in);
 
         int n = sc.nextInt();
+
         System.out.printf("%d", sum(n));
+        // Please write your code here.
     }
 
-    public static int sum(int n) {
-        // 음수 입력 처리
-        n = Math.abs(n);
-
-        if (n == 0) {
-            return 0; // 종료 조건
+    public static int sum(int n){
+        if(n == 0){
+            return 0;
         }
 
-        // 현재 자리 숫자의 제곱 + 나머지 처리
         return (n % 10) * (n % 10) + sum(n / 10);
     }
 }
